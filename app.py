a2e943cc0b17729f34bf4d45a75c0c57743ecf1f
sofia <kobernik.sofia@yandex.ru>
2025-07-31 23:12:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__, template_folder='templates')
 
@@ -6,6 +6,16 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/')
 def index():  # put application's code here
     return render_template('índex.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/day1')
+def day1():
+    return render_template('day1.html')
 
 
 if __name__ == '__main__':
