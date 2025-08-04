2fdd3da43d9b774bfb823010e6a08d99babeab48
sofia <kobernik.sofia@yandex.ru>
2025-08-04 22:26:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():  # put application's code here
-    return render_template('home.html')
+    return render_template('index.html')
 
 
 @app.route('/about')
